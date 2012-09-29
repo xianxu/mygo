fda6e87c394fe08a95b7dcab50febd521a963b75
Xian Xu <xx@twitter.com>
2012-09-28 20:52:38 -0700

    use builtin logger

--- src/tfe/confs/empty.go
+++ src/tfe/confs/empty.go
@@ -2,12 +2,12 @@ package confs
 
 import (
 	"tfe"
-	"code.google.com/p/log4go"
+	"log"
 )
 
 func init() {
 	ok := tfe.AddRules("empty", func() map[string]tfe.Rules { return make(map[string]tfe.Rules) })
 	if !ok {
-		log4go.Warn("Rule set named empty already exists")
+		log.Println("Rule set named empty already exists")
 	}
 }
